14698a84f69b36e318a89954220db736495874c1
Tales Coelho <tales-coelho@live.com>
2021-06-10 22:37:00 -0300

    portfolio no git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter } from "react-router-dom";
+import React, { useState } from 'react';
+import Pages from './components/Pages';
+import Navbar from './components/Navbar';
+import { FaRegMoon } from 'react-icons/fa';
+import { FiSun } from 'react-icons/fi';
+import "./App.css"
+
+function handleClick(mode) {
+  // console.log(mode)
+  if (mode) {
+    document.getElementById('root').parentNode.className = "dark";
+  } else {
+    document.getElementById('root').parentNode.className = "";
+  }
+}
 
 function App() {
+  const [state, setstate] = useState(0);
+  
+  if (!state) {
+    handleClick(true)
+  } else {
+    handleClick(false)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body-container">
+      <div className="main-content">
+        <div className="left">
+          <BrowserRouter>
+            <Navbar />
+            <Pages />
+          </BrowserRouter>
+        </div>
+        <div className="right">
+          <FaRegMoon className="mode" display={!state && "none"} size="2em" onClick={ () => setstate(0) } />
+          <FiSun className="mode" display={state && "none"} size="2em" onClick={ () => setstate(1) } />
+        </div>
+      </div>
     </div>
   );
 }
