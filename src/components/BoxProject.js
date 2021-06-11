ccabfc3e69520e0b82f89757ae780730850a60f7
Tales Coelho <tales-coelho@live.com>
2021-06-11 11:41:24 -0300

    portfolio no git

---
+++ src/components/BoxProject.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './css/Show.css';
+import './css/Projects.css';
+
+function BoxProject(props) {
+  console.log(props);
+  return (
+    <div>
+      <a
+        href={ props.url.route }
+        target="_blank"
+        rel="noopener noreferrer"
+        className="project-box link"
+      >
+        <img className="img-container" src={ props.url.url } alt="TrybeWarts"></img>
+      </a>
+    </div>
+  );
+}
+
+export default BoxProject;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import BoxProject from './BoxProject';
 import './css/Show.css';
+import './css/Projects.css';
 
 function Projects() {
+  const trybeWarts = {
+    url: "https://raw.githubusercontent.com/tryber/sd-011-project-trybewarts/e8361258a88e7b3491babf50bc4c640e257e52bb/images/trybewarts-colored.svg",
+    route: "https://github.com/tryber/sd-011-project-trybewarts/pull/113",
+  }
   return (
     <div className="show">
       <h1>
         Projetos
       </h1>
+      <div className="box-container">
+        <BoxProject url={ trybeWarts } />
+      </div>
     </div>
   )
 }
